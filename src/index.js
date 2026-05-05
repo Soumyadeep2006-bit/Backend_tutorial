6b2722072355dad94ee39a5e4bde6ec1dd8b304a
Soumyadeep <dassoumyadeep2006@gmail.com>
2026-05-06 03:26:13 +0530

    utils code discussion , apiResponse and apiError

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 
 import dotenv from "dotenv"
 
-dotenv.config({
-    path: "../.env"
-})
+dotenv.config()
 
 import connectDB from "./db/index.js"
 
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is listening at port :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err)
+})
